d54dbe19bbd14729725e35fdb8c38d7127d8254e
João Martins <jferreiram2000@gmail.com>
2021-01-11 16:26:43 +0000

    delete declared but not used object

--- src/entity/product.entity.ts
+++ src/entity/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { Product } from '../interfaces/product.interface';
 
 @Entity()
